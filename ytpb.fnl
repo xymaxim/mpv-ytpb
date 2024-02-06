59261ca8e29751422a400a54762785cf2cb03055
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2024-02-06 11:21:30 +0300

    feat: Add 'change seek offset' action

--- ytpb.fnl
+++ ytpb.fnl
@@ -1,5 +1,6 @@
 (local mp (require :mp))
 (local options (require :mp.options))
+(local input (require :mp.input))
 
 (local state {:osd (mp.create_osd_overlay :ass-events)})
 
@@ -11,7 +12,8 @@
   (set state.osd.data "{\\an4}{\\fnmonospace}{\\fs18}{\\b1}Rewind and seek
 {\\an4}{\\fnmonospace}{\\fs18}{\\b1}r{\\b0} {\\fs18}rewind
 {\\an4}{\\fnmonospace}{\\fs18}{\\b1}b{\\b0} seek backward
-{\\an4}{\\fnmonospace}{\\fs18}{\\b1}f{\\b0} seek forward\\N\\N
+{\\an4}{\\fnmonospace}{\\fs18}{\\b1}f{\\b0} seek forward
+{\\an4}{\\fnmonospace}{\\fs18}{\\b1}O{\\b0} change seek offset\\N\\N
 {\\an4}{\\fnmonospace}{\\fs18}{\\b1}Miscellaneous
 {\\an4}{\\fnmonospace}{\\fs18}{\\b1}s{\\b0} take a screenshot
 {\\an4}{\\fnmonospace}{\\fs18}{\\b1}q{\\b0} quit")
@@ -35,10 +37,21 @@
 (fn seek-backward-key-handler []
   (mp.commandv :script-message "yp:seek" (.. "-" settings.seek_offset)))
 
+(fn change-seek-offset-key-handler []
+  (input.get {:prompt "Seek offset:"
+              :default_text settings.seek_offset
+              :submit (fn [value]
+                        (if (string.find value "[dhms]")
+                            (do
+                              (set settings.seek_offset value)
+                              (input.terminate))
+                            (input.log_error "Invalid value, should be [%dd][%Hh][%Mm][%Ss]")))}))
+
 (fn activate []
   (tset key-binds :r [:rewind rewind-key-handler])
   (tset key-binds :b [:seek-backward seek-backward-key-handler])
   (tset key-binds :f [:seek-forward seek-forward-key-handler])
+  (tset key-binds :O [:change-seek-offset change-seek-offset-key-handler])
   (tset key-binds :s [:take-screenshot take-screenshot-key-handler])
   (tset key-binds :q [:quit deactivate])
   (let [do-and-deactivate (fn [func] (fn [] (func) (deactivate)))]
--- ytpb.lua
+++ ytpb.lua
@@ -1,10 +1,11 @@
 local mp = require("mp")
 local options = require("mp.options")
+local input = require("mp.input")
 local state = {osd = mp.create_osd_overlay("ass-events")}
 local settings = {seek_offset = "10m"}
 local key_binds = {}
 local function display_keys_overlay()
-  state.osd.data = "{\\an4}{\\fnmonospace}{\\fs18}{\\b1}Rewind and seek\n{\\an4}{\\fnmonospace}{\\fs18}{\\b1}r{\\b0} {\\fs18}rewind\n{\\an4}{\\fnmonospace}{\\fs18}{\\b1}b{\\b0} seek backward\n{\\an4}{\\fnmonospace}{\\fs18}{\\b1}f{\\b0} seek forward\\N\\N\n{\\an4}{\\fnmonospace}{\\fs18}{\\b1}Miscellaneous\n{\\an4}{\\fnmonospace}{\\fs18}{\\b1}s{\\b0} take a screenshot\n{\\an4}{\\fnmonospace}{\\fs18}{\\b1}q{\\b0} quit"
+  state.osd.data = "{\\an4}{\\fnmonospace}{\\fs18}{\\b1}Rewind and seek\n{\\an4}{\\fnmonospace}{\\fs18}{\\b1}r{\\b0} {\\fs18}rewind\n{\\an4}{\\fnmonospace}{\\fs18}{\\b1}b{\\b0} seek backward\n{\\an4}{\\fnmonospace}{\\fs18}{\\b1}f{\\b0} seek forward\n{\\an4}{\\fnmonospace}{\\fs18}{\\b1}O{\\b0} change seek offset\\N\\N\n{\\an4}{\\fnmonospace}{\\fs18}{\\b1}Miscellaneous\n{\\an4}{\\fnmonospace}{\\fs18}{\\b1}s{\\b0} take a screenshot\n{\\an4}{\\fnmonospace}{\\fs18}{\\b1}q{\\b0} quit"
   return (state.osd):update()
 end
 local function deactivate()
@@ -28,26 +29,38 @@ end
 local function seek_backward_key_handler()
   return mp.commandv("script-message", "yp:seek", ("-" .. settings.seek_offset))
 end
+local function change_seek_offset_key_handler()
+  local function _3_(value)
+    if string.find(value, "[dhms]") then
+      settings.seek_offset = value
+      return input.terminate()
+    else
+      return input.log_error("Invalid value, should be [%dd][%Hh][%Mm][%Ss]")
+    end
+  end
+  return input.get({prompt = "Seek offset:", default_text = settings.seek_offset, submit = _3_})
+end
 local function activate()
   key_binds["r"] = {"rewind", rewind_key_handler}
   key_binds["b"] = {"seek-backward", seek_backward_key_handler}
   key_binds["f"] = {"seek-forward", seek_forward_key_handler}
+  key_binds["O"] = {"change-seek-offset", change_seek_offset_key_handler}
   key_binds["s"] = {"take-screenshot", take_screenshot_key_handler}
   key_binds["q"] = {"quit", deactivate}
   do
     local do_and_deactivate
-    local function _3_(func)
-      local function _4_()
+    local function _5_(func)
+      local function _6_()
         func()
         return deactivate()
       end
-      return _4_
+      return _6_
     end
-    do_and_deactivate = _3_
-    for key, _5_ in pairs(key_binds) do
-      local _each_6_ = _5_
-      local name = _each_6_[1]
-      local func = _each_6_[2]
+    do_and_deactivate = _5_
+    for key, _7_ in pairs(key_binds) do
+      local _each_8_ = _7_
+      local name = _each_8_[1]
+      local func = _each_8_[2]
       mp.add_forced_key_binding(key, name, do_and_deactivate(func))
     end
   end
