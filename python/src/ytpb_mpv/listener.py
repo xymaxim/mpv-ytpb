4ffb2032c334430af777652b4e0e81f5392c435e
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2024-04-10 17:08:35 +0300

    Remove RewindTreeMap in favor of added Playback.rewind_history

--- python/src/ytpb_mpv/listener.py
+++ python/src/ytpb_mpv/listener.py
@@ -20,71 +20,6 @@ logger = structlog.get_logger(__name__)
 YTPB_CLIENT_NAME = "yp"
 
 
-@dataclass
-class RewindTreeNode:
-    key: Timestamp
-    value: SegmentSequence
-    left: Self | None = None
-    right: Self | None = None
-
-
-@dataclass
-class RewindTreeMap:
-    """A binary search tree implementation to store key-value pairs.
-
-    Keys represent timestamps of segments, while values are sequence numbers.
-    """
-
-    root: RewindTreeNode | None = None
-
-    @staticmethod
-    def _is_tree_node(node: RewindTreeNode | None) -> TypeGuard[RewindTreeNode]:
-        return node is not None
-
-    @staticmethod
-    def _insert(
-        node: RewindTreeNode | None, key: Timestamp, value: SegmentSequence
-    ) -> RewindTreeNode:
-        if not RewindTreeMap._is_tree_node(node):
-            return RewindTreeNode(key, value, None, None)
-        else:
-            if key < node.key:
-                left = RewindTreeMap._insert(node.left, key, value)
-                return RewindTreeNode(node.key, node.value, left, node.right)
-            elif key > node.key:
-                right = RewindTreeMap._insert(node.right, key, value)
-                return RewindTreeNode(node.key, node.value, node.left, right)
-            else:
-                return RewindTreeNode(node.key, value, node.left, node.right)
-
-    def insert(self, key: Timestamp, value: SegmentSequence) -> None:
-        """Insert a pair of timestamp and sequence number into the tree."""
-        self.root = RewindTreeMap._insert(self.root, key, value)
-
-    @staticmethod
-    def _closest(
-        node: RewindTreeNode | None, target: Timestamp, closest: RewindTreeNode
-    ) -> RewindTreeNode | None:
-        if not RewindTreeMap._is_tree_node(node):
-            return closest
-        else:
-            result = closest
-            if abs(target - closest.key) > abs(target - node.key):
-                result = node
-            if target < node.key:
-                return RewindTreeMap._closest(node.left, target, result)
-            elif target > node.key:
-                return RewindTreeMap._closest(node.right, target, result)
-            else:
-                return result
-
-    def closest(self, target: Timestamp) -> RewindTreeNode | None:
-        """Find the node closest to the target timestamp."""
-        if self.root is None:
-            return None
-        return RewindTreeMap._closest(self.root, target, self.root)
-
-
 class Listener:
     def __init__(
         self, ipc_server: Path, playback: Playback, streams: SetOfStreams
@@ -96,8 +31,6 @@ class Listener:
 
         self._mpv = MPV(start_mpv=False, ipc_socket=str(ipc_server))
         self._mpv.bind_event("client-message", self._client_message_handler)
-
-        self.rewind_tree = RewindTreeMap()
 
     def _client_message_handler(self, event: dict) -> None:
         logger.debug(event)
@@ -143,29 +76,10 @@ class Listener:
         return mpd_path, mpd_start_time
 
     def handle_rewind(self, target_date: datetime) -> None:
-        some_base_url = next(iter(self._streams)).base_url
-
-        target = target_date.timestamp()
-        if reference := self.rewind_tree.closest(target):
-            reference_sequence = reference.value
-        else:
-            reference_sequence = None
-
-        sl = SegmentLocator(
-            some_base_url,
-            reference_sequence=reference_sequence,
-            temp_directory=self._playback.get_temp_directory(),
-            session=self._playback.session,
+        moment = self._playback.locate_moment(target_date)
+        rewound_segment = self._playback.get_segment(
+            moment.sequence, next(iter(self._streams)).base_url
         )
-        locate_result = sl.find_sequence_by_time(target)
-        rewound_segment = self._playback.get_downloaded_segment(
-            locate_result.sequence, some_base_url
-        )
-
-        self.rewind_tree.insert(
-            rewound_segment.metadata.ingestion_walltime, locate_result.sequence
-        )
-
         self._mpd_path, self._mpd_start_time = self._compose_mpd(
             rewound_segment.metadata
         )
@@ -176,7 +90,7 @@ class Listener:
             "script-message",
             "yp:rewind-completed",
             str(self._mpd_path),
-            str(locate_result.time_difference),
+            str(moment.cut_at),
         )
 
     def start(self) -> None:
@@ -185,10 +99,8 @@ class Listener:
         latest_sequence = request_reference_sequence(
             some_base_url, self._playback.session
         )
-        latest_segment = self._playback.get_downloaded_segment(
-            latest_sequence, some_base_url
-        )
-        self.rewind_tree.insert(
+        latest_segment = self._playback.get_segment(latest_sequence, some_base_url)
+        self._playback.rewind_history.insert(
             latest_segment.metadata.ingestion_walltime, latest_sequence
         )
 
